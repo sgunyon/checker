c0037c004404a4c316f0a115db90b672aa4e122b
sgunyon <7231693+sgunyon@users.noreply.github.com>
2018-02-20 19:59:16 -0800

    Added live node tracking and excluded failed operations

--- check_consistency.py
+++ check_consistency.py
@@ -5,6 +5,8 @@ class Consistency():
         self.old_nodes = 0
         self.new_nodes = 0
         self.live_nodes = 0
+        self.new_live_nodes = 0
+
 
     def consistency(self, dictionary):
         time = dictionary['time']
@@ -17,26 +19,26 @@ class Consistency():
             'module' : module,
         }
 
-        if module == 'topology':
-            self.output['node state'] = self.update_topology(action[0], action[1])
-
-        if module == 'write':
-            self.update_write_list(action)
-            if self.check_level(action):
-                 self.output['consistency'] = 'Consistency check succeeded.'
-            else:
-                self.output['consistency'] = 'Consistency check failed.'
-            self.output['logging'] = self.validate_logging(self.output['consistency'], log)
-
-        if module == 'read':
+        if module == 'read' or module == 'write':
+            if module == 'write':
+                self.update_write_list(action)
             if self.check_level(action):
                 self.output['consistency'] = 'Consistency check succeeded.'
-                self.output['data validation'] = self.validate_reads(action)
+                if module == 'read':
+                    self.output['data validation'] = self.validate_reads(action)
             else:
                 self.output['consistency'] = 'Consistency check failed.'
+            self.output['log validation'] = self.validate_logging(self.output['consistency'], log)
 
-        if module == 'live_nodes':
-            self.output['live nodes'] = self.update_live_nodes(action[1])
+        if not log == 'fail':
+            if module == 'topology':
+                self.output['cluster state'] = self.update_topology(action[0], action[1])
+
+            if module == 'live_nodes':
+                self.output['live nodes'] = self.update_live_nodes(action)
+        else:
+            if not module == 'write':
+                self.output['report'] = 'Operation failed'
 
         self.create_output(self.output)
 
@@ -44,11 +46,11 @@ class Consistency():
         self.old_nodes = old_node_update
         self.new_nodes = new_node_update
 
-        if self.old_nodes == 0 or self.live_nodes > self.new_nodes:
-            self.live_nodes = self.new_nodes
+        if self.old_nodes == 0 or self.new_live_nodes > self.new_nodes:
+            self.new_live_nodes = self.new_nodes
 
         if self.new_nodes > self.old_nodes and self.old_nodes > 0:
-            self.live_nodes = self.live_nodes + (self.new_nodes - self.old_nodes)
+            self.new_live_nodes = self.new_live_nodes + (self.new_nodes - self.old_nodes)
 
         return (self.old_nodes, self.new_nodes)
 
@@ -56,11 +58,11 @@ class Consistency():
         consistency_level = action[0]
         if len(action) > 2:
             if consistency_level == 'ALL':
-                return self.new_nodes > 0 and self.new_nodes == self.live_nodes
+                return self.new_nodes > 0 and self.new_nodes == self.new_live_nodes
             if consistency_level == 'QUORUM':
-                return self.live_nodes >= ((self.new_nodes // 2) + 1)
+                return self.new_live_nodes >= ((self.new_nodes // 2) + 1)
             if consistency_level == 'ONE':
-                return self.live_nodes >= 1
+                return self.new_live_nodes >= 1
         return False
 
     def update_write_list(self, action):
@@ -73,13 +75,16 @@ class Consistency():
         return 'No correpsonding write.'
 
     def update_live_nodes(self, live_node_update):
-        self.live_nodes = live_node_update
-        return self.live_nodes
+        if live_node_update[0] == self.new_live_nodes:
+            self.new_live_nodes = live_node_update[1]
+            return self.new_live_nodes
+        return 'Live node count error.'
+
 
     def validate_logging(self, consistency, log):
         if consistency == 'Consistency check succeeded.' and log == 'ok':
             return 'Logging confirmed'
-        if consistency == 'Consistency check failed.' and log == 'failed':
+        if consistency == 'Consistency check failed.' and log == 'fail':
             return 'Logging confirmed'
         return 'Logging failed'
 
